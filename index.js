ba0352f7be6dde9b7a41d194333d98eb95084327
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-09 21:19:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 // Write your solution in this file!
 var customerName
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
